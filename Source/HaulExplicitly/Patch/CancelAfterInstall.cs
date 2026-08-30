bbbeb060d774921be6edfeef2dfb1fdc0bc6f896
Keahuy <58843468+Keahuy@users.noreply.github.com>
2026-08-30 14:12:45 +0800

    fix:禁止搬运的未安装建筑在安装后仍保留禁止搬运标记

---
+++ Source/HaulExplicitly/Patch/CancelAfterInstall.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using HaulExplicitly.Extension;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace HaulExplicitly.Patch;
+
+[HarmonyPatch(typeof(JobDriver_HaulToContainer), "ModifyPrepareToil")]
+public class CancelAfterInstall
+{
+    
+    [HarmonyPostfix]
+    static void AddStep (JobDriver_HaulToContainer __instance, Toil toil)
+    {
+        toil.AddFinishAction(delegate
+        {
+            __instance.ThingToCarry.SetDontMoved(false);
+        });
+    }
+}
